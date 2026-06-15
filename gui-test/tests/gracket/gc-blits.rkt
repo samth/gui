bf16968b81386d7e460fa2e8d66ec905bbd8c8fd
Sam Tobin-Hochstadt <samth@iu.edu>
2026-06-15 17:36:46 -0400

    Fix GTK collecting blit window cleanup

--- gui-test/tests/gracket/gc-blits.rkt
+++ gui-test/tests/gracket/gc-blits.rkt
@@ -8,15 +8,18 @@
 (define off (make-bitmap w h))
 (send (send on make-dc) draw-rectangle 5 5 (- w 10) (- h 10))
 
-(send
- (for/fold ([prev-f #f]) ([i (in-range 10)])
-   (define f (new frame% [label "GC Blit"] [width 100] [height 100]))
-   (define c (new canvas% [parent f]))
-   (register-collecting-blit c 0 0 w h on off)
-   (send f show #t)
-   (collect-garbage)
-   (yield (system-idle-evt))
-   (when prev-f (send prev-f show #f))
-   f)
- show #f)
+(define final
+  (for/fold ([prev #f]) ([i (in-range 10)])
+    (define f (new frame% [label "GC Blit"] [width 100] [height 100]))
+    (define c (new canvas% [parent f]))
+    (register-collecting-blit c 0 0 w h on off)
+    (send f show #t)
+    (collect-garbage)
+    (yield (system-idle-evt))
+    (when prev
+      (unregister-collecting-blit (cdr prev))
+      (send (car prev) show #f))
+    (cons f c)))
 
+(unregister-collecting-blit (cdr final))
+(send (car final) show #f)
